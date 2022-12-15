9d12e3ea9961e656fe9f084f860a1de4220fe5cd
unknown <mohanvamshi96@gmail.com>
2022-12-16 02:39:28 +0530

    git pages commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ networkCanvas.width=300;
 const carCtx=carCanvas.getContext("2d");
 const networkCtx=networkCanvas.getContext("2d");
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-const N=1;
+const N=1000;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -92,7 +92,6 @@ function animate(time){
 
     bestCar.draw(carCtx,"blue",true);
     carCtx.restore();
-
     networkCtx.lineDashOffset=-time/50;
     Visualizer.drawNetwork(networkCtx,bestCar.brain);
     requestAnimationFrame(animate);
